84260957ccd726dd1cf763e28acac05bdc2e39e6
Ramosh99 <einfetch@gmail.com>
2024-09-04 23:13:08 +0530

    updated

--- back/obj/Debug/net8.0/Back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/Back.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+666e08e5057f5be12c979fb27c8bc81e71bd876b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cc13a2ba887e4e28065864e1992d6cf4cf2063")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
